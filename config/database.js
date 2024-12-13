12dc5206c33aa7009624554bbc3fb8456c444f6f
Akash Dewangan <103268574+akash-dewangan03@users.noreply.github.com>
2024-12-13 19:57:59 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -5,8 +5,9 @@ exports.connect = () => {
   console.log("MongoDB URI:", process.env.MONGODB_URI);
   
   mongoose.connect(process.env.MONGODB_URI, {
-      // useNewUrlParser: true,
-      // useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      connectTimeoutMS: 20000,
     })
     .then(() => console.log("DB Connected Successfully"))
     .catch((error) => {
